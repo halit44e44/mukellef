0b4ce918acea3f0e5450dc19e003871de42816be
halitdogan <halit.dogan@clasital.com>
2023-07-24 02:28:09 +0300

    transaction complated

--- app/Services/UserSubscriptionService.php
+++ app/Services/UserSubscriptionService.php
@@ -3,15 +3,20 @@
 
 namespace App\Services;
 
+use App\Repositories\TransactionRepository;
 use App\Repositories\UserSubscriptionRepository;
 
 class UserSubscriptionService
 {
+    private $result;
+    private bool $paymentStatus = true;
     protected UserSubscriptionRepository $userSubscriptionRepository;
+    protected TransactionRepository $transactionRepository;
 
-    public function __construct(UserSubscriptionRepository $userSubscriptionRepository)
+    public function __construct(UserSubscriptionRepository $userSubscriptionRepository, TransactionRepository $transactionRepository)
     {
         $this->userSubscriptionRepository = $userSubscriptionRepository;
+        $this->transactionRepository = $transactionRepository;
     }
 
     public function createOrUpdateSubscription(array $data)
@@ -20,10 +25,22 @@ class UserSubscriptionService
             throw new \Exception('This user has a subscription', 400);
         }
 
-        if ($checkSub = $this->userSubscriptionRepository->findByUserId($data['user_id'])) {
-            return $this->userSubscriptionRepository->update($checkSub->id, $data);
+        //TODO:: Ödeme için gerekli servislere istekler atılmış olarak kabul ediyorum.
+        if ($this->paymentStatus) {
+            if ($checkSub = $this->userSubscriptionRepository->findByUserId($data['user_id'])) {
+                $this->result = $this->userSubscriptionRepository->update($checkSub->id, $data);
+            } else {
+                $this->result = $this->userSubscriptionRepository->create($data);
+            }
+            $this->transactionRepository->create([
+                'user_subscription_id' => $this->result->id,
+                'price' => $this->result->subscription->price,
+                'result' => $this->paymentStatus
+            ]);
+
+            return $this->result;
         }
-        return $this->userSubscriptionRepository->create($data);
+        return false;
     }
 
 
